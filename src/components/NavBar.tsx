25983448cb5a27233537e8ff668a063b613ee250
Andrey <55235312+andrew133@users.noreply.github.com>
2021-11-01 06:45:44 +0300

    014

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,20 +1,23 @@
 import React from "react";
+import './NavBar.module.css';
+import s from "./NavBar.module.css";
 
 const NavBar = () => {
-    return  <nav className='nav'>
-        <div>
+    return  <nav className={s.nav}
+    >
+        <div className={s.item}>
             <a>Profile</a>
         </div>
-        <div>
+        <div className={`${s.item} ${s.active}`}>
             <a>Messages</a>
         </div>
-        <div>
+        <div className={s.item}>
             <a>News</a>
         </div>
-        <div>
+        <div className={s.item}>
             <a>Music</a>
         </div>
-        <div>
+        <div className={s.item}>
             <a>Settings</a>
         </div>
     </nav>
--- src/components/Profile.tsx
+++ src/components/Profile.tsx
@@ -1,7 +1,9 @@
 import React from "react";
+import './Profile.module.css';
+import s from "./Profile.module.css";
 
 const Profile = () => {
-    return  <div className='content'>
+    return  <div className={s.content}>
         <div>
             <img src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT2cGUqGnCYT6DxUTYNd8hqbRLcDCZ9c6TsEw&usqp=CAU'/>
         </div>
@@ -13,11 +15,13 @@ const Profile = () => {
             <div>
                 New posts
             </div>
-            <div>
+            <div className={s.posts}>
+            <div className={s.item}>
                 post 1
             </div>
-            <div>
+            <div className={s.item}>
                 post 2
+            </div>
             </div>
         </div>
     </div>
