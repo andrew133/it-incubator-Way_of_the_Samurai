01320df5e36537d11baf8ab9b3938905115d5139
Andrey <55235312+andrew133@users.noreply.github.com>
2021-11-02 15:37:00 +0300

    016

--- src/components/Profile/Profile.tsx
+++ src/components/Profile/Profile.tsx
@@ -1,30 +1,20 @@
 import React from "react";
 import './Profile.module.css';
 import s from "./Profile.module.css";
+import MyPosts from "./MyPost/MyPosts";
 
 const Profile = () => {
-    return  <div className={s.content}>
+    return (
+    <div className={s.content}>
         <div>
             <img src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT2cGUqGnCYT6DxUTYNd8hqbRLcDCZ9c6TsEw&usqp=CAU'/>
         </div>
         <div>
             ava + description
         </div>
-        <div>
-            My posts
-            <div>
-                New posts
-            </div>
-            <div className={s.posts}>
-            <div className={s.item}>
-                post 1
-            </div>
-            <div className={s.item}>
-                post 2
-            </div>
-            </div>
-        </div>
+       <MyPosts />
     </div>
+    )
 }
 
 export default Profile;
