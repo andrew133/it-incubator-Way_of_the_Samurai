2982972d55ce12542eac8af5c3931c5361c80c62
Andrey <55235312+andrew133@users.noreply.github.com>
2021-11-22 16:58:01 +0300

    019

--- src/App.tsx
+++ src/App.tsx
@@ -4,20 +4,23 @@ import Header from "./components/Header/Header";
 import NavBar from "./components/NavBar/NavBar";
 import Profile from "./components/Profile/Profile";
 import Dialogs from "./components/Dialogs/Dialogs";
-
-
+import {BrowserRouter, Route, Routes} from 'react-router-dom';
 
 function App() {
     return (
-        <div className='app-wrapper'>
-            <Header />
-            <NavBar />
-            {/*<Profile />*/}
-            <div className='app-wrapper-content'>
-                <Dialogs />
+        <BrowserRouter>
+            <div className='app-wrapper'>
+                <Header/>
+                <NavBar/>
+                <div className='app-wrapper-content'>
+                    <Routes>
+                        <Route path = "/dialogs" element={<Dialogs />}/>
+                        <Route path = "/profile" element={<Profile />}/>
+                    </Routes>
+                </div>
             </div>
+        </BrowserRouter>
 
-        </div>
     );
 }
 
--- src/components/NavBar/NavBar.tsx
+++ src/components/NavBar/NavBar.tsx
@@ -6,10 +6,10 @@ const NavBar = () => {
     return  <nav className={s.nav}
     >
         <div className={s.item}>
-            <a>Profile</a>
+            <a href="/profile">Profile</a>
         </div>
         <div className={`${s.item} ${s.active}`}>
-            <a>Messages</a>
+            <a href="/dialog">Messages</a>
         </div>
         <div className={s.item}>
             <a>News</a>
